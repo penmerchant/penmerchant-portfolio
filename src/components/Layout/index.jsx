9f86ef3078dd6b875871caa06ac19963cadd9a42
penmerchant <aniqnoob666@gmail.com>
2023-03-19 19:29:44 +0800

    first version

---
+++ src/components/Layout/index.jsx
@@ -0,0 +1,12 @@
+import tw from "twin.macro";
+
+const Navbar = () => {
+    return <Header>
+        <p>
+            sad
+        </p>
+    </Header>
+};
+
+const Header = tw.div`w-full bg-white`;
+export default Navbar;
