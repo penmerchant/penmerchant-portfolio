e2f0065d1261293cd233f4782aca33ab29d22d25
penmerchant <aniqnoob666@gmail.com>
2023-03-21 16:53:26 +0800

    added font onto project

--- src/pages/HeroSection/index.jsx
+++ src/pages/HeroSection/index.jsx
@@ -18,8 +18,8 @@ const HeroSection = () => {
 const HeroContainer = tw.div`h-screen w-full bg-[#40222d] grid place-content-center items-center bg-no-repeat bg-cover
     max-[600px]:grid-cols-1
     max-[600px]:bg-auto`;
-const InfoWrapper = tw.div`h-auto w-auto text-white opacity-80 bg-[#40222d]`;
-const HeadingName = tw.h2`leading-3 p-4`;
-const ParagraphInfo = tw.p`leading-3 p-4`;
-const Button = tw.button`h-[50px] w-full bg-[#CC6840] text-white outline-0 border-0 hover:bg-[#a35333] `;
+const InfoWrapper = tw.div`h-auto w-auto text-white opacity-80 bg-[#40222d] font-['Stay pixel']`;
+const HeadingName = tw.h2`leading-3 p-4 text-6xl`;
+const ParagraphInfo = tw.p`leading-3 p-4 text-lg`;
+const Button = tw.button`h-[50px] w-full bg-[#CC6840] text-lg text-white outline-0 border-0 hover:bg-[#a35333] font-['Stay pixel']`;
 export default HeroSection;
